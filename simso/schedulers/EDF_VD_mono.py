d5d2b5b6f2b09f06bc8847d3f40b9b9594866f09
nicolasbenatti <nicolas.benatti@mail.polimi.it>
2025-06-24 22:14:51 +0200

    Add EDF-VD Mixed-Criticality scheduler

---
+++ simso/schedulers/EDF_VD_mono.py
@@ -0,0 +1,69 @@
+"""
+Uniprocessor Earliest Deadline First algorithm, with Virtual Deadlines for Mixed-Criticality scheduling.
+"""
+from simso.core import Scheduler
+from simso.schedulers import scheduler
+from simso.core.Task import MCPTask
+from simso.utils.MixedCriticality import CritLevel
+
+
+@scheduler("simso.schedulers.EDF_VD_mono")
+class EDF_VD_mono(Scheduler):
+    def init(self):
+        self.ready_list = []
+
+        for t in self.task_list:
+            assert isinstance(t, MCPTask), \
+                "EDF-VD can only schedule Mixed-Criticality tasks."
+
+    def on_activate(self, job):
+        if job.task.criticality_level == CritLevel.HI:
+            Ulo_lo = self.system_utilization_at_level(CritLevel.LO, CritLevel.LO)
+            Uhi_hi = self.system_utilization_at_level(CritLevel.HI, CritLevel.HI)
+
+            vd = self.vd_coeff
+
+            if Ulo_lo + Uhi_hi > 1:
+                # Apply virtual deadlines
+                job.absolute_deadline *= vd
+
+        self.ready_list.append(job)
+        job.cpu.resched()
+
+    def on_terminated(self, job):
+        self.ready_list.remove(job)
+        job.cpu.resched()
+
+    def schedule(self, cpu):
+        if self.ready_list:
+            # job with the highest priority
+            job = min(self.ready_list, key=lambda x: x.absolute_deadline)
+        else:
+            job = None
+
+        return (job, cpu) 
+    
+    def system_utilization_at_level(self, k: CritLevel, j: CritLevel) -> float:
+        """
+        Computes system utilization at crit. level `k` for
+        `j`-criticality tasks.
+        """
+        res = 0.0
+        for t in self.task_list:
+            if t.criticality_level == j:
+                if k == CritLevel.LO:
+                    res += float(t.wcet) / t.period
+                else:
+                    res += float(t.wcet_hi) / t.period
+
+        return res
+
+    @property
+    def vd_coeff(self):
+        """
+        Coefficient for virtual deadlines.
+        """
+        Ulo_lo = self.system_utilization_at_level(CritLevel.LO, CritLevel.LO)
+        Ulo_hi = self.system_utilization_at_level(CritLevel.LO, CritLevel.HI)
+
+        return Ulo_hi / (1 - Ulo_lo)
