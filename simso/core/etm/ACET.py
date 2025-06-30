b547302aefba7c1dc100fc24b5fea368b64ac65c
nicolasbenatti <nicolas.benatti@mail.polimi.it>
2025-06-30 17:15:14 +0200

    Add mixed-criticality version of ACET ETM

--- simso/core/etm/ACET.py
+++ simso/core/etm/ACET.py
@@ -1,5 +1,5 @@
 from simso.core.etm.AbstractExecutionTimeModel \
-    import AbstractExecutionTimeModel
+    import (AbstractExecutionTimeModel, MCAbstractExecutionTimeModel)
 import random
 
 # TODO: the seed should be specified in order to evaluate on identical systems.
@@ -35,6 +35,66 @@ class ACET(AbstractExecutionTimeModel):
 
     def on_preempted(self, job):
         self.update_executed(job)
+
+    def on_terminated(self, job):
+        self.update_executed(job)
+        del self.et[job]
+
+    def on_abort(self, job):
+        self.update_executed(job)
+        del self.et[job]
+
+    def get_executed(self, job):
+        if job in self.on_execute_date:
+            c = (self.sim.now() - self.on_execute_date[job]) * job.cpu.speed
+        else:
+            c = 0
+        return self.executed[job] + c
+
+    def get_ret(self, job):
+        return int(self.et[job] - self.get_executed(job))
+
+    def update(self):
+        for job in list(self.on_execute_date.keys()):
+            self.update_executed(job)
+
+class MC_ACET(MCAbstractExecutionTimeModel):
+    def __init__(self, sim, *_):
+        print("MC_ACET")
+        MCAbstractExecutionTimeModel.__init__(self, sim)
+        self.sim = sim
+        self.et = {}
+        self.on_execute_date = {}
+
+    def init(self):
+        pass
+
+    def update_executed(self, job):
+        if job in self.on_execute_date:
+            self.executed[job] += (self.sim.now() - self.on_execute_date[job]
+                                   ) * job.cpu.speed
+
+            del self.on_execute_date[job]
+
+    def on_activate(self, job):
+        MCAbstractExecutionTimeModel.on_activate(self, job)
+        self.executed[job] = 0
+        self.et[job] = min(
+            job.task.wcet,
+            random.normalvariate(job.task.acet, job.task.et_stddev)
+        ) * self.sim.cycles_per_ms
+
+    def on_execute(self, job):
+        self.on_execute_date[job] = self.sim.now()
+
+    def on_preempted(self, job):
+        self.update_executed(job)
+
+    def on_mode_switch(self, job, new_crit_level):
+        if new_crit_level == 'HI':
+            self.curr_wcet[job] = job.wcet_hi
+        else:
+            self.curr_wcet[job] = job.wcet
 
     def on_terminated(self, job):
         self.update_executed(job)
--- simso/core/etm/__init__.py
+++ simso/core/etm/__init__.py
@@ -1,5 +1,5 @@
 from .WCET import WCET
-from .ACET import ACET
+from .ACET import (ACET, MC_ACET)
 from .Apriori import Apriori
 from .CacheModel import CacheModel
 from .FixedPenalty import FixedPenalty
@@ -7,6 +7,7 @@ from .FixedPenalty import FixedPenalty
 execution_time_models = {
     'wcet': WCET,
     'acet': ACET,
+    'mc_acet': MC_ACET,
     'apriori': Apriori,
     'cache': CacheModel,
     'fixedpenalty': FixedPenalty
@@ -15,6 +16,7 @@ execution_time_models = {
 execution_time_model_names = {
     'WCET': 'wcet',
     'ACET': 'acet',
+    'MC_ACET': 'mc_acet',
     'Cache Model': 'cache',
     'Fixed Penalty': 'fixedpenalty'
 }
